9635252c8e6f5d2ff99f84644cb7c748a8385eb3
alisson0507tga-max <alisson0507tga@gmail.com>
2026-08-22 21:33:18 -0400

    Bump GPSSpeed to version 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.alisson.gpsspeed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
     }
 
     buildTypes {
