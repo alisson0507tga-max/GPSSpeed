b4387fd8b672fe206d045ecafa424a452e5a2015
alisson0507tga-max <alisson0507tga@gmail.com>
2026-08-24 13:24:50 -0400

    Bump GPSSpeed to 1.7.0 automatic timeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.alisson.gpsspeed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.6.0"
+        versionCode = 11
+        versionName = "1.7.0"
     }
 
     buildTypes {
